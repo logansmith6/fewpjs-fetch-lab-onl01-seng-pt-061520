3ab991f5c2b63175d88dcddbf99430791a0f0d63
Logan Smith <logsmith06@gmail.com>
2021-01-04 17:57:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+
 function fetchBooks() {
 
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json));
 }
 
-function renderBooks(books) {
+function renderBooks(json) {
   const main = document.querySelector('main')
-  books.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
@@ -13,4 +17,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+
 })
